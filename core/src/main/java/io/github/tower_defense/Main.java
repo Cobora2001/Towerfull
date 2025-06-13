dd98e2d624677af37523298b33dcfe06fd710d7a
Cobora2001 <thomasvu@bluewin.ch>
2025-06-13 20:31:54 +0200

    add music

--- core/src/main/java/io/github/tower_defense/Main.java
+++ core/src/main/java/io/github/tower_defense/Main.java
@@ -1,14 +1,23 @@
 package io.github.tower_defense;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import io.github.tower_defense.screen.MainMenuScreen;
 import io.github.tower_defense.tools.GameAssets;
 
 public class Main extends Game {
+    private Music bgMusic;
+
     @Override
     public void create() {
         // Initialize the game assets
         GameAssets.get().loadAll();
+
+        // Start background music
+        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Age_of_war_theme.mp3"));
+        bgMusic.setLooping(true);
+        bgMusic.play();
 
         this.setScreen(new MainMenuScreen(this));
     }
@@ -17,6 +26,12 @@ public class Main extends Game {
     public void dispose() {
         // Dispose of game assets
         GameAssets.get().dispose();
+
+        // Stop and dispose of background music
+        if (bgMusic != null) {
+            bgMusic.stop();
+            bgMusic.dispose();
+        }
 
         // Call the super dispose method
         super.dispose();
---
+++ core/src/main/java/io/github/tower_defense/screen/accessories/GameUI.java
@@ -0,0 +1,4 @@
+package io.github.tower_defense.screen.accessories;
+
+public class GameUI {
+}
